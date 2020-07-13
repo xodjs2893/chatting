109d9f0b563d109f6a5c730a6043da9c29c438e5
jintaeeon <xodjs2893@naver.com>
2020-07-13 16:06:19 +0900

    'chatting'

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const { Socket } = require('dgram');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+
+
 
 app.set('views', `${__dirname}/public`)
 app.set('view engin', 'html')
 app.engine('html', require('ejs').renderFile)
-app.use(express.static('src', {
+app.use(express.static('public', {
     extensicons: ['html']
 }))
 
+io.on('connection',function(socket){
+    console.log('User connected...');
+    socket.on('chatting',(data)=>{
+        io.sockets.emit('chatting',data)
+    })
+})
 
 
 app.get('/', (req, res) => {
+    console.log(__dirname);
     res.render('index');
 })
 app.post('/api/users', (req, res) => {
@@ -22,6 +34,7 @@ app.post('/api/users', (req, res) => {
     res.send(users)
 })
 
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
-})
+// app.listen(PORT, () => {
+//     console.log(`server is running on ${PORT}`);
+// })
+http.listen(PORT,()=>console.log(`listening on localhost:${PORT}`));
